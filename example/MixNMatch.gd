b15c6efb92734161259e4f0defc56568a9234d74
Lyndon Tan <pikadon92@gmail.com>
2020-06-27 19:53:54 +0800

    Fully-implemented Types
    Have Arceus change type based on the button pressed.

--- example/MixNMatch.gd
+++ example/MixNMatch.gd
@@ -26,28 +26,18 @@ var cur_type
 var final_palette:LargeTexture
 
 func _ready():
-	types = [type_normal, type_fighting, type_flying, type_poison, type_ground, type_rock, type_bug, type_ghost, type_steel, type_fire, type_water, type_grass, type_electric, type_ice, type_psychic, type_dragon, type_dark, type_fairy]
+	types = [type_normal, type_fighting, type_flying, type_poison, type_ground, type_rock, type_bug, type_ghost, type_steel, type_fire, type_water, type_grass, type_electric, type_psychic, type_ice, type_dragon, type_dark, type_fairy]
 	
 	cur_shininess = normal
 	cur_type = types[0]
-	set_shader_combo()
 	pass
 
 func _on_CheckButton_toggled(button_pressed):
 	cur_shininess = shiny if button_pressed else normal
-	#set_shader_combo()
 	get_material().set_shader_param("palette_shininess", cur_shininess)
 	pass
 
 func _on_typeBtn_pressed(type_i):
 	cur_type = types[type_i]
-	#set_shader_combo()
 	get_material().set_shader_param("palette_type", cur_type)
-	pass
-
-func set_shader_combo():
-	#final_palette = cur_shininess * cur_type
-	#final_palette.add_piece(Vector2(), cur_shininess)
-	#final_palette.add_piece(Vector2(0, 1), cur_type)
-	get_material().set_shader_param("palette_shininess", cur_shininess)
 	pass
